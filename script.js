19b033503ec7367c0eac526dc5576d9d6ecfe010
thusharathiwanka <thusharathiwank123@gmail.com>
2020-06-28 23:05:10 +0530

    added filter

--- script.js
+++ script.js
@@ -2,10 +2,12 @@
 const taskInput = document.querySelector(".task-input");
 const addBtn = document.querySelector(".add-btn");
 const taskList = document.querySelector(".task-list");
+const filterOption = document.querySelector(".filter-task");
 
 //Adding event listeners
 addBtn.addEventListener("click", addTask);
 taskList.addEventListener("click", taskDeleteCheck);
+filterOption.addEventListener("click", filterTask);
 
 //Creating function for add tasks
 function addTask(event) {
@@ -42,15 +44,53 @@ function addTask(event) {
   }
 }
 
-//Creating function for delete task
+//Creating function for delete and check task
 function taskDeleteCheck(element) {
   const clickElement = element.target;
 
   if (clickElement.classList[0] === "remove-btn") {
-    clickElement.parentElement.remove();
+    clickElement.parentElement.classList.add("deleted");
+    /* Setting timeout for calling remove or
+    add event listener of transition end */
+    setTimeout(() => {
+      clickElement.parentElement.remove();
+    }, 500);
   }
 
   if (clickElement.classList[0] === "complete-btn") {
     clickElement.parentElement.classList.toggle("completed");
   }
+}
+
+//Creating function for filtering
+function filterTask(element) {
+  //Selecting all divs
+  const tasks = taskList.querySelectorAll("div");
+  //Checking each div and adding styles according to class name
+  tasks.forEach((task) => {
+    if (element.target.value === "all") {
+      task.style.display = "flex";
+      return;
+    }
+    //Checking if select menu name equals to completed
+    if (element.target.value === "completed") {
+      //Checking for elements with completed class name
+      if (task.classList.contains("completed")) {
+        task.style.display = "flex";
+        return;
+      } else {
+        task.style.display = "none";
+        return;
+      }
+    }
+    if (element.target.value !== "completed") {
+      if (!task.classList.contains("completed")) {
+        task.style.display = "flex";
+        return;
+      } else {
+        task.style.display = "none";
+        return;
+      }
+    }
+  });
 }
