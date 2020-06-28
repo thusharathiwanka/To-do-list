58b985dac68979fd5c186592423423f277ab945e
thusharathiwanka <thusharathiwank123@gmail.com>
2020-06-28 16:11:50 +0530

    added remove functionality

---
+++ script.js
@@ -0,0 +1,50 @@
+//Selecting elements form HTML document
+const taskInput = document.querySelector(".task-input");
+const addBtn = document.querySelector(".add-btn");
+const taskList = document.querySelector(".task-list");
+
+//Adding event listeners
+addBtn.addEventListener("click", addTask);
+taskList.addEventListener("click", deleteTask);
+
+//Creating function for add tasks
+function addTask(event) {
+  //Prevent form from submitting
+  event.preventDefault();
+  //Creating div
+  const taskDiv = document.createElement("div");
+  taskDiv.classList.add("task");
+
+  //Creating li
+  const newTask = document.createElement("li");
+  newTask.innerText = taskInput.value;
+  newTask.classList.add("task-item");
+  taskDiv.appendChild(newTask);
+
+  //Adding check button
+  const completeBtn = document.createElement("button");
+  completeBtn.classList.add("complete-btn");
+  completeBtn.innerHTML = "<li class='fas fa-check'></li>";
+  taskDiv.appendChild(completeBtn);
+
+  //Adding remove button
+  const removeBtn = document.createElement("button");
+  removeBtn.classList.add("remove-btn");
+  removeBtn.innerHTML = "<li class='fas fa-trash'></li>";
+  taskDiv.appendChild(removeBtn);
+
+  //Append div to list
+  taskList.appendChild(taskDiv);
+
+  //Clearing input value
+  taskInput.value = "";
+}
+
+//Creating function for delete task
+function deleteTask(element) {
+  const clickElement = element.target;
+
+  if (clickElement.classList[0] === "remove-btn") {
+    clickElement.parentElement.remove();
+  }
+}
